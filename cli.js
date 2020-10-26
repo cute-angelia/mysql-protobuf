ecb091c896697cae2c53e8a8b3be5c66c7543c5a
vanilla <rose20.99.c@gmail.com>
2020-10-26 21:26:19 +0800

    [update] mkdir

--- cli.js
+++ cli.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 var args = process.argv.splice(2)
 var convert = require('./')
 var fs = require('fs')
@@ -7,9 +8,13 @@ var file = args[0]
 var data = convert(fs.readFileSync(file).toString())
 process.stdout.write(data.protobuf)
 
-fs.writeFile(data.name + ".proto", data.protobuf, function (err) {
+var path = data.name + "/" + data.name + ".proto";
+
+fs.mkdirSync("./" + data.name + "/");
+
+fs.writeFile(path, data.protobuf, function (err) {
   if (err) {
     return console.log(err);
   }
-  console.log("文件保存在 -> " + data.name + ".proto");
+  console.log("文件保存在 -> " + path);
 });
